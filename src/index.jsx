44c6d2d36c829caa1234ca38f9503c3c0b8eda47
Diego Garces Jimenez <diegogarcesj1@gmail.com>
2021-10-05 12:36:06 -0300

    Class-based component with changes in state

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 import '../assets/stylesheets/application.scss';
 
+/*
 const Hello = ({ name }) => {
   return (
     <div>
@@ -11,6 +12,27 @@ const Hello = ({ name }) => {
     </div>
   );
 };
+*/
+
+class Hello extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { clicked: false };
+  }
+
+  handleClick = () => {
+    this.setState({ clicked: !this.state.clicked });
+  }
+
+  render() {
+    return (
+      <h1 className={this.state.clicked ? "clicked" : null } onClick={this.handleClick}>
+        Hello, {this.props.name}
+      </h1>
+    );
+  }
+}
 
 const root = document.getElementById('root');
 if (root) {
