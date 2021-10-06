6c48348e23e238d28727066ebc110f4612579e29
Diego Garces Jimenez <diegogarcesj1@gmail.com>
2021-10-06 10:57:45 -0300

    Improvements

--- src/components/gif.jsx
+++ src/components/gif.jsx
@@ -7,6 +7,9 @@ class Gif extends Component {
 
   render() {
     const src = `https://media3.giphy.com/media/${this.props.id}/giphy.gif`;
+    if (!this.props.id) {
+      return null;
+    }
     return (
       <img src={src} alt="" className="gif" onClick={this.handleClick} />
     );
--- src/components/search_bar.jsx
+++ src/components/search_bar.jsx
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
+  }
+
   handleUpdate = (event) => {
+    this.setState({ term: event.target.value });
     this.props.searchFunction(event.target.value);
     if (event.target.value === '') {
       this.props.selectFunction('');
@@ -11,6 +18,7 @@ class SearchBar extends Component {
   render() {
     return(
       <input type="text" className="form-control form-search"
+      value={this.state.term}
       onChange={this.handleUpdate} />
     );
   }
